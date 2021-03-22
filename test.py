cb966ebcc55c275e89975c06da79c4ddc6b0d8cf
SheikhAizaz <80956078+SheikhAizaz@users.noreply.github.com>
2021-03-22 22:46:53 +0500

    Update test.py

--- test.py
+++ test.py
@@ -27,7 +27,7 @@ def main():
         st.image(image, caption="Simple", use_column_width=True)
         components.html(
             """ 
-        <script src="http://maps.google.com/maps/api/js?sensor=false"></script>
+        
         <script src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.10.1.min.js"></script>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
